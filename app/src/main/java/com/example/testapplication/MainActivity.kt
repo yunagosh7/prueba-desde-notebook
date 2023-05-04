368d207d43cd896a107bd0491f52fbb4b9a87f06
yunagosh <yunagosh@gmail.com>
2023-05-04 19:03:46 -0300

    First commit

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.testapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            binding.textView.text = binding.editText.text.toString()
+        }
+
+
+
+
+    }
+}
